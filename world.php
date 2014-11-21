fa0355e027e3db9252c7d98be973bcef6ae739da
chanzrobe <chanz.robe@gmail.com>
2014-11-21 14:22:20 -0500

    Update world.php

--- world.php
+++ world.php
@@ -9,10 +9,22 @@ $LOOKUP = $_REQUEST['lookup'];
 # execute a SQL query on the database
 $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
 print $results;
-# loop through each country
-while ($row = mysql_fetch_array($results)) {
-  ?>
-  <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
-  <?php
+if (mysql_num_rows($result) > 0)
+{
+   $doc = new_xmldoc("1.0"); // create DomDocument object
+   
+   $root = $doc->add_root("countrydata"); //add root node
+
+  while ($row = mysql_fetch_array($results)){ //loop through each country
+      $country = $root->new_child("country", "");     // create item node
+      
+      // attach name and ruler as children of item node
+      $record->new_child("name", $row["name"]);
+      $record->new_child("ruler", $row["head_of_state"]);
+   }
+
+  // print the tree 
+  echo $doc->dumpmem();
 }
 ?>
+
