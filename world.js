a3d402f9b2b5ec90aced4253f5c570ed8aa0e63c
chanzrobe <chanz.robe@gmail.com>
2014-11-21 14:25:25 -0500

    Update world.js

--- world.js
+++ world.js
@@ -6,27 +6,33 @@ function ajaxFunction(){
 		if(ajaxRequest.readyState == 4){
         	var xmlDocument = ajaxRequest.responseXML;
         	var xmlDoc = ajaxRequest.responseText;
-        	//alert(xmlDoc);
-			var parser=new DOMParser();
-  			var xmlDoc2=parser.parseFromString(xmlDoc,"text/xml");
+        	alert(xmlDocument);
+			//var parser=new DOMParser();
+  			//var xmlDoc2=parser.parseFromString(xmlDoc,"text/xml");
         	//alert(xmlDoc2);
-			var next = "";
-			var text = xmlDoc2.getElementsByTagName("li");
+			//var next = "";
+			//var text = xmlDoc2.getElementsByTagName("li");
 			//alert(text.length);
-			for (var i=0; i<text.length; i++){
-				next += text[i].value;
-			}
-			document.getElementById("result").innerHTML=next;
-			alert(next);
+			//for (var i=0; i<text.length; i++){
+				//next += text[i].value;
+			//}
+			//document.getElementById("result").innerHTML=next;
+			//alert(next);
 		}
 	}
+	var check = document.getElementById("all").checked;
+	alert(check)
 	var lookup = document.getElementById('term').value;
-	if(lookup === ""){
+	
+	if(lookup === "" && check === false){
 		alert("Invalid input");
 	}
-	else{
+	else if(check === true && lookup === ""){
+		ajaxRequest.open("GET", "world.php?all=", true);
+		ajaxRequest.send(null);
+	}
+	else if(check === false && lookup !== ""){
 		ajaxRequest.open("GET", "world.php?lookup="+lookup, true);
-		ajaxRequest.setRequestHeader('Content-Type', 'text/xml');
 		ajaxRequest.send(null);
 	}
 }
